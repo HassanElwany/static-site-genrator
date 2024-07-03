ff00a9f8aaefdbca34cabb4ada84cb3eb25793c3
HassanElwany <elwany_hassan@hotmail.com>
2024-07-03 20:34:57 +0300

    final edit

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,7 +1,6 @@
 import os
 from markdown_blocks import markdown_to_html_node
 
-
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
@@ -24,7 +23,7 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
-
+    to_file.close()
 
 def extract_title(md):
     lines = md.split("\n")
@@ -32,3 +31,13 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("No title found")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for root, _, files in os.walk(dir_path_content):
+        for file in files:
+            if file.endswith('.md'):
+                from_path = os.path.join(root, file)
+                relative_path = os.path.relpath(from_path, dir_path_content)
+                dest_path = os.path.join(dest_dir_path, relative_path)
+                dest_path = os.path.splitext(dest_path)[0] + '.html'
+                generate_page(from_path, template_path, dest_path)
